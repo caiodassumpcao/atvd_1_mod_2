09cf8ae2eebb564a32f1022fe117ff4e4495bb39
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-21 15:56:36 -0300

    Recebendo o raio da esfera

---
+++ questao_10/src/questao_10.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class questao_10 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o raio da esfera:");
+        int raio = sc.nextInt();
+        
+        sc.close();
+
+        
+    }
+}

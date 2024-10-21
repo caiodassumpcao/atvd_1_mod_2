2805a50034e09ec057549f6b2bd829ef9087b5ae
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-21 16:04:35 -0300

    Volume da esfera

--- questao_10/src/questao_10.java
+++ questao_10/src/questao_10.java
@@ -5,10 +5,13 @@ public class questao_10 {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Digite o raio da esfera:");
-        int raio = sc.nextInt();
+        double raio = sc.nextInt();
         
         sc.close();
 
-        
+        double volume = Math.PI * Math.pow(raio, 3) * 4 / 3;
+
+        System.out.printf("Volume da esfera: %.2f\n", volume);
+
     }
 }

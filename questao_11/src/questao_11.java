7d1ed2f5f5e58a49f1f5b9ece71371e592be674b
caiodassumpcao <caio.rodri.d@gmail.com>
2024-10-21 16:16:56 -0300

    Imprimindo arae do triangulo

--- questao_11/src/questao_11.java
+++ questao_11/src/questao_11.java
@@ -1,5 +1,16 @@
-public class App {
+import java.util.Scanner;
+
+public class questao_11 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+                Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a base do triangulo:");
+        double base = sc.nextInt();
+        System.out.println("Digite a altura do triangulo:");
+        double altura = sc.nextInt();
+        sc.close();
+
+        double area = base * altura / 2;
+        System.out.printf("A área do triangulo: %.2f\n", area);
     }
 }
